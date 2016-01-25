6904db782eea91e0e11fae9e316d65a1e9bc1804
Konrad Pason <konrad.pason17@gmail.com>
2016-01-25 15:47:52 +0100

    Fixed the problem with the other site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+  get 'welcome/services'
+
+  get 'welcome/srvices'
+
+  get 'welcome/about'
+
+  get 'welcome/index'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get about" do
+    get :about
     assert_response :success
   end
 
